2b3e388e829bfb46f2c6f169f3e9817ff60e0645
JulieBodette <jr3python@gmail.com>
2024-03-13 15:36:34 -0400

    Added Wisej Hybrid Project with button

---
+++ WisejHybridLocalApplication1/Page1.cs
@@ -0,0 +1,26 @@
+﻿using Wisej.Web;
+using Wisej.Hybrid;
+
+namespace WisejHybridLocalApplication1
+{
+	public partial class Page1 : Page
+	{
+		public Page1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, System.EventArgs e)
+		{
+			if (Device.Valid)
+			{
+				Device.Popups.DisplayAlert("Congrats", "You Win");
+			}
+			else
+			{
+				AlertBox.Show("You Win");
+			}
+
+		}
+	}
+}
